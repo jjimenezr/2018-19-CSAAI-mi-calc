c57ff9c3811b2d83aaf06e8fc44ad6ba37398e37
Javier Jimenez Ramiro <jjimenez@beta06.aulas.gsyc.urjc.es>
2019-02-28 12:31:06 +0100

    calculadora terminada

--- mijs.js
+++ mijs.js
@@ -11,11 +11,18 @@ function main() {
     boton8 : document.getElementById('boton8'),
     boton9 : document.getElementById('boton9'),
     boton0 : document.getElementById('boton0'),
+    botoncoma : document.getElementById('botoncoma'),
     botonsuma : document.getElementById('botonsuma'),
     botonresta : document.getElementById('botonresta'),
     botonmultiplicacion : document.getElementById('botonmultiplicacion'),
     botondivision : document.getElementById('botondivision'),
+    botonAC : document.getElementById('botonAC'),
     display : document.getElementById('display'),
+    botonigual : document.getElementById('botonigual'),
+    operator1 : 0,
+    operator2 : 0,
+    operation : null,
+    operationstate : null,
   }
   gui.boton1.onclick = () => {
     var x = 1;
@@ -110,5 +117,62 @@ function main() {
   gui.boton0.onclick = () => {
     var x = 0;
     gui.display.innerHTML += x;
+  }
+
+  gui.botoncoma.onclick = () => {
+    var x = ".";
+    var pos = gui.display.innerHTML.indexOf("0");
+    if (pos == 0){
+        gui.display.innerHTML = gui.display.innerHTML.replace("0","0.");
+    }else{
+        gui.display.innerHTML += x;
+    }
+  }
+
+  gui.botonAC.onclick = () => {
+    gui.operator1 = 0;
+    gui.operator2 = 0;
+    gui.operation = null;
+    gui.operationstate = null;
+    gui.display.innerHTML = 0; 
+  }
+
+  gui.botonsuma.onclick = () => {
+    gui.operator1 = gui.display.innerHTML;
+    gui.display.innerHTML = 0;
+    gui.operationstate = "suma";
+  }
+  gui.botonresta.onclick = () => {
+    gui.operator1 = gui.display.innerHTML;
+    gui.display.innerHTML = 0;
+    gui.operationstate = "resta";
+  }
+  gui.botonmultiplicacion.onclick = () => {
+    gui.operator1 = gui.display.innerHTML;
+    gui.display.innerHTML = 0;
+    gui.operationstate = "multiplicacion";
+  }
+  gui.botondivision.onclick = () => {
+    gui.operator1 = gui.display.innerHTML;
+    gui.display.innerHTML = 0;
+    gui.operationstate = "division";
+  }
+  gui.botonigual.onclick = () =>{
+    gui.operator2 = gui.display.innerHTML;
+    if (gui.operationstate == "suma") {
+      gui.operation = Number(gui.operator1) + Number(gui.operator2);
+      gui.display.innerHTML = gui.operation;
+    } else if (gui.operationstate == "resta") {
+      gui.operation = Number(gui.operator1) - Number(gui.operator2);
+      gui.display.innerHTML = gui.operation;
+    } else if (gui.operationstate == "multiplicacion") {
+      gui.operation = Number(gui.operator1) * Number(gui.operator2);
+      gui.display.innerHTML = gui.operation;
+    } else if (gui.operationstate == "division") {
+      gui.operation = Number(gui.operator1) / Number(gui.operator2);
+      gui.display.innerHTML = gui.operation;
+    }
+    gui.operation = 0;
+    gui.operationstate = null;
   }
 }
